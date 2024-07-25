e6c09843cf0c5f9a5b5877300de8560e479f9c4f
Guz_dev <guz.developer@gmail.com>
2024-07-25 15:38:13 -0400

    estructura inicial del proyecto

---
+++ src/app/components/dashboards/prices/prices.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prices',
+  standalone: true,
+  imports: [],
+  templateUrl: './prices.component.html',
+  styleUrl: './prices.component.css'
+})
+export class PricesComponent {
+
+}
