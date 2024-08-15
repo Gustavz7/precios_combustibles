59b233702e6ae7c7af62b50bcf4875a53139a3ae
Guztavz7 <guz.developer@gmail.com>
2024-08-14 23:30:40 -0400

    agregado angular-material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 //now in angular@17 the libraries is imported here
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideAnimationsAsync(),
   ]
 };
